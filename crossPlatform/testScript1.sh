3a484dd981deec02b7175003519d2654ae986725
Markus Schlueter <mschlueter@geomar.de>
2020-04-27 14:23:21 +0200

    added sleep to testscripts for the cluster-execution-script

--- crossPlatform/testScript1.sh
+++ crossPlatform/testScript1.sh
@@ -2,3 +2,5 @@
 
 echo "this is the execution of testScript 1 on $(whoami)!";
 echo "params to this script: $@"
+
+sleep 4
--- crossPlatform/testScript2.sh
+++ crossPlatform/testScript2.sh
@@ -2,3 +2,5 @@
 
 echo "this is the execution of testScript 2 on $(whoami)!";
 echo "params to this script: $@"
+
+sleep 4
