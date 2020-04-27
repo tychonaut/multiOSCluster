3eff6ea6b45657b866346dd2304dc9e3b381812a
Markus Schlueter <mschlueter@geomar.de>
2020-04-27 20:04:00 +0200

    deleted files; forgot a 'git' before the mv command, hence add/delete now

--- windows/secondaryNode/rebootToWindows.ps1
+++
@@ -1,8 +0,0 @@
-
-#change working directory to that of the script, to call sub-scripts with relative paths
-#https://stackoverflow.com/questions/4724290/powershell-run-command-from-scripts-directory
-$scriptpath = $MyInvocation.MyCommand.Path
-$scriptdir = Split-Path $scriptpath
-cd "$scriptdir"
-
-.\rebootToSelectedOS "Windows"
