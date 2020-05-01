011f71e17785aa494fe2cb2acb24d5680aa2b722
Markus Schlueter <mschlueter@geomar.de>
2020-05-01 21:40:28 +0200

    added folders for varius OpenSpace profiles that each contain config and calibration files. They are to be rsynced before app launching, hence overcoming the problem of having to change the same file for each node. As a side product, requirement of accessing the NAS can be omitted.

---
+++ appLaunch/OpenSpace/rsyncOpenSpaceInstallToCluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# usage: $0 [ [-a|--all] | [-m|--master|--masters]  [-s|--slaves] ] 
+# 
+
+
+# repository directory is one folder above this script
+REPO_DIRECTORY="$( readlink -f $( dirname $0 )/.. )"
+
+# recursive, compress during transfer, human readable, verbose, show progress, -e: specify rmote shell (ssh)
+
+# don't sync files with names read from the following file
+rsyncignoreFileName="${REPO_DIRECTORY}/manage/.rsyncignore"
+
+clusterNodeTypeFlags=""
+if [[ $# > 0 ]]; then
+    clusterNodeTypeFlags=$1
+fi
+
+
+${REPO_DIRECTORY}/helpers/rsyncToCluster.sh ${clusterNodeTypeFlags} --ignorefile "${rsyncignoreFileName}" --source-path "${REPO_DIRECTORY}"
