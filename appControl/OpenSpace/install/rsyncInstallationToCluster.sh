40206f1c0ef174bf97d1843f76c014acef7b20cf
Markus Schlueter <mschlueter@geomar.de>
2020-05-02 02:28:35 +0200

    everything prepared for first rsync to cluster of an Openspace installation

--- appControl/OpenSpace/install/rsyncInstallationToCluster.sh
+++ appControl/OpenSpace/install/rsyncInstallationToCluster.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# usage: $0 [ [-a|--all] | [-m|--master|--masters]  [-s|--slaves] ] 
-# 
+# This script copies the whole contents of the multiOSCluster repository 
+# to all the cluster machines specified in {REPO_DIRECTORY}/config/hosts.json including masters
+# (without git meta files, which are excluded via ${REPO_DIRECTORY}/manage/.rsyncignore).
+# If the running machine is itself part of the cluster (including being a master), self-sending is omitted.
+# If it is not a master (e.g.  a remote develeoper machine, masters will also be targeted)
+
+SCRIPT_DIRECTORY=$( readlink -f $( dirname $0 ))
+# repository directory is THREE folders above this script
+REPO_DIRECTORY="$( readlink -f $( dirname $0 )/../../.. )"
 
 
-# repository directory is one folder above this script
-REPO_DIRECTORY="$( readlink -f $( dirname $0 )/.. )"
-
-# recursive, compress during transfer, human readable, verbose, show progress, -e: specify rmote shell (ssh)
+# recursive, compress during transfer, human readable, verbose, show progress, -e: specify remote shell (ssh)
 
 # don't sync files with names read from the following file
-rsyncignoreFileName="${REPO_DIRECTORY}/manage/.rsyncignore"
-
-clusterNodeTypeFlags=""
-if [[ $# > 0 ]]; then
-    clusterNodeTypeFlags=$1
-fi
+rsyncignoreFileName="${SCRIPT_DIRECTORY}/.rsyncignore"
 
 
-${REPO_DIRECTORY}/helpers/rsyncToCluster.sh ${clusterNodeTypeFlags} --ignorefile "${rsyncignoreFileName}" --source-path "${REPO_DIRECTORY}"
+openSpaceInstallDirectory=$( jq '.apps.Windows.OpenSpace.installDir' ${REPO_DIRECTORY}/config/apps.json | ${REPO_DIRECTORY}/helpers/stripLeadingAndTrailingQuotes.sh )
+
+
+echo "OpenSpace Install Directory to rsync to cluster: ${openSpaceInstallDirectory}"
+sleep 3
+
+#${REPO_DIRECTORY}/helpers/rsyncToCluster.sh ${clusterNodeTypeFlags} --ignorefile "${rsyncignoreFileName}" --source-path "${REPO_DIRECTORY}"
+${REPO_DIRECTORY}/helpers/rsyncToCluster.sh --ignorefile "${rsyncignoreFileName}" --source-path "${openSpaceInstallDirectory}"
