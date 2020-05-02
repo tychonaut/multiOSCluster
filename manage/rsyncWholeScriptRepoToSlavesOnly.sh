e32c4a8714ed24fc5fe962fc1cfe86103d5bfaab
Markus Schlueter <mschlueter@geomar.de>
2020-05-02 02:07:12 +0200

    deleted now-obsolete rsyncWholeScriptRepoToSlavesOnly.sh

--- manage/rsyncWholeScriptRepoToSlavesOnly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Convenience script to quickly sync the current machine's (propably master) multiOSCluster-repository to all slaves;
-
-SCRIPT_DIRECTORY="$(dirname $0 )"
-
-${SCRIPT_DIRECTORY}/rsyncWholeScriptRepoToCluster.sh --slaves
