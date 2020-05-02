ab903ba97422533c5d18f542faca91513cdb4a0a
Markus Schlueter <mschlueter@geomar.de>
2020-05-03 00:12:34 +0200

    3/5 projectors show a correctly warped and blended image. RT1 has not woken up after a restart, and RT2's recndering is extremly broken; investigating...

--- appControl/OpenSpace/workaround/setPrimaryDisplayToHDMIProjector.sh
+++ appControl/OpenSpace/workaround/setPrimaryDisplayToHDMIProjector.sh
@@ -5,8 +5,10 @@
 #not neccecary for master
 #ssh ARENAMASTER+arena@arenamaster "/D/apps/PSTools/PsExec64.exe -i \\\\$(hostname) -d -s  D:\\apps\\multimonitortool-x64\\MultiMonitorTool.exe //SetPrimary 2"
  
-ssh ARENART1+arena@arenart1 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart1 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 2"
-ssh ARENART2+arena@arenart2 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart2 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 3"
+ echo "THIS SCRIPT $0 IS SO HACKY, FIX ASAP"
+
+#ssh ARENART1+arena@arenart1 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart1 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 2"
+ssh ARENART2+arena@arenart2 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart2 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 2"
 ssh ARENART3+arena@arenart3 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart3 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 3"
 ssh ARENART4+arena@arenart4 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart4 -d -s  D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 3"
 ssh ARENART5+arena@arenart5 "/D/apps/PSTools/PsExec64.exe -i \\\\\\\\arenart5 -d -s D:\\\\apps\\\\multimonitortool-x64\\\\MultiMonitorTool.exe //SetPrimary 3"
