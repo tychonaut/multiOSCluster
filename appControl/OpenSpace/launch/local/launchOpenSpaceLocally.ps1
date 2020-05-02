97b139a7bbfb2451a9dced9bac60d6ccf6b11c58
Markus Schlueter <mschlueter@geomar.de>
2020-05-02 14:55:52 +0200

    troublesome commit due to PSexec64.exe again failing to work when needing network access; plus, rt1 and master aren't reachable anymore; at least the json config pays off now

--- appControl/OpenSpace/launch/local/launchOpenSpaceLocally.ps1
+++ appControl/OpenSpace/launch/local/launchOpenSpaceLocally.ps1
@@ -17,11 +17,16 @@ echo "exeToLaunch: $exeToLaunch"
 # https://superuser.com/questions/199387/elevated-command-line-prompt-cant-access-shared-drives
 # the next command seems needed after a reboot:
 
-echo "enabling access to network share S:"
+#echo "enabling access to network share S:"
 #D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s powershell.exe net use S: \\10.0.10.6\Shared /user:nasuser rackstation1 /Y
-D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s powershell.exe $PSScriptRoot\..\..\..\..\helpers\accessNetworkShares.ps1
+D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -s powershell.exe $PSScriptRoot\..\..\..\..\helpers\accessNetworkShares.ps1
 
 echo "launching OpenSpace on $(hostname)..."
 #D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s D:\devel\OpenSpace\OpenSpace_ownDeploy\bin\Release\OpenSpace.exe
+#D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s $installDir/$exeToLaunch
+
 D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s $installDir/$exeToLaunch
 
+#D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d -s D:/apps/OpenSpace/v0.15.1_git_arena/bin/OpenSpace.exeD:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d  D:/apps/OpenSpace/v0.15.1_git_arena/bin/OpenSpace.exe
+
+#D:\apps\PSTools\PsExec64.exe -i \\$(hostname) -d  D:/apps/OpenSpace/v0.15.1_git_arena/bin/OpenSpace.exe
