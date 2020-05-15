819f9a055fa2fb3c8d04e0c72fd0982d7c4e6e10
Markus Schlueter <mschlueter@geomar.de>
2020-05-15 04:56:54 +0200

    found a hacky way to  workaround that enables launching a pvserver cluster from the local paraview client instance on a remote machine: in a bash script, SSH to master, then PsExec'ing the mpiexec command with username and password; create a windows link to the bash script, enter 'powershell.exe <the windows link to bash script>' as a launch command to default_servers.pvsc. Simple, right? ;( Again: call paraview client -> parse default_servers.pvsc -> call powershell on link to bash script -> open bash -> ssh to remote master -> enable network access via net use -> start psexec -> start mpiexec -> launch all pvserver-instances

--- appControl/ParaView/profiles/default/launchClusterServers.ps1
+++ appControl/ParaView/profiles/default/launchClusterServers.ps1
@@ -1,1 +1,5 @@
+#cd 'C:\Program Files\Microsoft MPI\Bin'
+
+#.\mpiexec.exe -np 2 -machinefile D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\pvserver.exe D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\dome_arena.pvx
+
 mpiexec.exe -np 2 -machinefile D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\pvserver.exe D:\apps\ParaView\v5.8.0_git_official_CAVE\bin\dome_arena.pvx
---
+++ appControl/ParaView/profiles/default/launchClusterServers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+#jq-on-mys-workaround
+PATH="/d/apps/PSTools:/mingw64/bin/:$PATH"
+
+
+launchClusterServers()
+{
+    local appName="${1}"
+    
+    local SCRIPT_DIRECTORY="$( readlink -f $( dirname $0 ))"
+     # repository directory is four folders above this script's location
+    local REPO_DIRECTORY="$( readlink -f ${SCRIPT_DIRECTORY}/../../../.. )"
+
+    echo " launching MPI paraview server on remote master, then cascading to cluster..."
+
+    local exe_unixStyle="/D/apps/ParaView/v5.8.0_git_official_CAVE/bin/launchClusterServers.ps1"
+    local exe_windowsStyle=$( cygpath.exe -w -m "${exe_unixStyle}" )
+
+    #parse OS credentials:
+    # this construct requires the whole script repo to reside on each computer, and in the same directory!
+    local hostCredPath="${REPO_DIRECTORY}/helpers/getHostCreds.sh"
+
+
+    #this one works 0o:
+    ${REPO_DIRECTORY}/manage/performOnClusterNodes.sh --masters -NAS --execute-command "/D/apps/PSTools/PsExec64.exe -i -d \\\\\\\\\$(hostname) -h -u \$(${hostCredPath} username) -p \$(${hostCredPath} pw) mpiexec.exe -np 2 -machinefile D:\\\\apps\\\\ParaView\\\\v5.8.0_git_official_CAVE\\\\bin\\\\machines.txt  D:\\\\apps\\\\ParaView\\\\v5.8.0_git_official_CAVE\\\\bin\\\\pvserver.exe D:\\\\apps\\\\ParaView\\\\v5.8.0_git_official_CAVE\\\\bin\\\\dome_arena.pvx"
+
+
+    echo "done launching MPI paravie server on remote master, then cascading to cluster..."
+    sleep 3
+
+}
+
+
+launchClusterServers ParaView
+
