ba4361f89b1ad7ccffecff055a8f0a90085cad8b
Markus Schlueter <mschlueter@geomar.de>
2020-05-16 01:04:51 +0200

    openspace xml containing correct frustum data in FOV/EulerAngle format has its relevant parts parsed into octave via Xerces 2 Java' xml parser

--- appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
+++ appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
@@ -1,7 +1,74 @@
 
+javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xercesImpl.jar")
+javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xml-apis.jar")
+
+file_path = fileparts(mfilename('fullpath'))
+
+filename = strcat( file_path, "/openspace_sgct_config.xml")
 
 
-theta_degrees = 90 
-phi_degrees = 0 
+## These three lines are equivalent to xDoc = xmlread(filename) in Matlab
+parser = javaObject("org.apache.xerces.parsers.DOMParser");
+parser.parse(filename);
+xDoc = parser.getDocument();
 
-point_1 = sph2cart(theta_degrees/180*pi, phi_degrees/180*pi, 1)
+numFrusta = xDoc.getElementsByTagName("PlanarProjection").getLength();
+
+frusta_FoVEuler = [] ;
+
+# skip index 0, as we are not interested in the master node
+for frustumIndex = 1 : (numFrusta-1)
+  
+  planarProjElem = xDoc.getElementsByTagName("PlanarProjection").item(frustumIndex) ;
+ 
+  childNodes = planarProjElem.getChildNodes();
+
+  
+   fov = struct ( "up", 10, "down", 20, "left", 30, "right", 40 ) ;
+   eulerAngles = struct ( "yaw", 0, "pitch", 0 , "roll", 0) ;
+  
+  for childIndex = 0 : (childNodes.getLength() - 1)
+      
+    childNode = childNodes.item(childIndex);
+      
+    if(strcmp(childNode.getNodeName(), "FOV"))
+      
+      fovAttribs = childNode.getAttributes();
+      
+      fov = struct ( 
+        "up",     fovAttribs.getNamedItem("up").getNodeValue(),
+        "down",   fovAttribs.getNamedItem("down").getNodeValue(),
+        "left",   fovAttribs.getNamedItem("left").getNodeValue(),
+        "right",  fovAttribs.getNamedItem("right").getNodeValue()
+      )
+      
+    endif
+    
+    if(strcmp(childNode.getNodeName(), "Orientation"))
+    
+      dirAttribs = childNode.getAttributes();
+      
+      eulerAngles = struct (
+        "yaw",    dirAttribs.getNamedItem("heading").getNodeValue(),
+        "pitch",  dirAttribs.getNamedItem("pitch").getNodeValue(),
+        "roll",   dirAttribs.getNamedItem("roll").getNodeValue()
+      )
+      
+    endif
+    
+  endfor
+
+  frustum_FoVEuler = struct("fov", fov, "eulerAngles", eulerAngles)
+  
+  frusta_FoVEuler = [frusta_FoVEuler, frustum_FoVEuler]
+  
+endfor
+
+
+
+
+
+#theta_degrees = 90 
+#phi_degrees = 0 
+
+#point_1 = sph2cart(theta_degrees/180*pi, phi_degrees/180*pi, 1)
