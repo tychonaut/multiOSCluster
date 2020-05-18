b3bcdc6a9eb2cdfaaefe5c2deb70d0fdc61129f0
Markus Schlueter <mschlueter@geomar.de>
2020-05-18 12:24:06 +0200

    including host arenart3 to cluster

--- appControl/ParaView/profiles/default/bin/launchClusterServers.ps1
+++ appControl/ParaView/profiles/default/bin/launchClusterServers.ps1
@@ -4,5 +4,7 @@
 
 #--reverse-connection --client-host=arenamaster
 # --log=myPvserverLog.txt,9
+# --mpi 
+# --force-onscreen-rendering 
 
-mpiexec.exe -np 2 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe --mpi --force-onscreen-rendering  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
+mpiexec.exe -np 3 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
