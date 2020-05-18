cd57c7b29e6658f19284029612da6b1efc6bc164
Markus Schlueter <mschlueter@geomar.de>
2020-05-18 21:02:09 +0200

    finding a roatioan matrix in the octave repos that doe actal right handed rotation was the toughest part of implementing the conversion math (FOV+Euler -> plane corners)

--- appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
+++ appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
@@ -10,6 +10,9 @@
 # Elements of type <Machine Name="arenart<n>" > Elements, n in 1..5
 
 #-------------------------------------------------------------------------------
+pkg load matgeom
+#pkg load linear-algebra
+
 
 javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xercesImpl.jar")
 javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xml-apis.jar")
@@ -18,7 +21,7 @@ javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xml-apis.jar")
 file_path = fileparts(mfilename('fullpath'))
 
 
-filename_in = strcat( file_path, "/openspace_sgct_config.xml")
+filename_in = strcat( file_path, "/frusta_FOVEulerAngles_in.xml")
 ## These three lines are equivalent to xDoc_in = xmlread(filename_in) in Matlab
 parser_in = javaObject("org.apache.xerces.parsers.DOMParser");
 parser_in.parse(filename_in);
@@ -70,9 +73,9 @@ for frustumIndex = 1 : (numFrusta_in-1)
     
   endfor
 
-  frustum_FOVEuler = struct("fov", fov, "eulerAngles", eulerAngles)
+  frustum_FOV_Euler = struct("fov", fov, "eulerAngles", eulerAngles);
   
-  frusta_FOV_Euler = [frusta_FOV_Euler, frustum_FOVEuler]
+  frusta_FOV_Euler = [frusta_FOV_Euler, frustum_FOV_Euler];
   
 endfor
 
@@ -82,31 +85,56 @@ endfor
 # do the conversions :
 
 
+frusta_planeCorners = [];
+for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
+ 
+  printf("frustum #: %d\n", frustumIndex_in)
+ 
+  fovU = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.up));
+  fovD = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.down));
+  fovL = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.left));
+  fovR = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.right));
 
-for frustumIndex_in = 0 : numFrusta_out
-
-
-
+  ll = [ -tan(fovL), -tan(fovD), -1.0]' ;
+  lr = [  tan(fovR), -tan(fovD), -1.0]' ;
+  ur = [  tan(fovR),  tan(fovU), -1.0]' ;
   
+  #yaw pitch roll -> rotate about y,x,z
+  yaw =   deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.yaw));
+  pitch = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.pitch));
+  roll =  deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.roll));
   
+  yawMat   = createRotationOy( yaw );
+  pitchMat = createRotationOx( pitch );
+  rollMat  = createRotationOz( roll );
   
-
+  rotationMat = rollMat * pitchMat * yawMat;
+  rotationMat = rotationMat(1:3,1:3) 
+  
+  planeCorners = struct(
+    "LowerLeft",  rotationMat * ll,
+    "LowerRight", rotationMat * lr,
+    "UpperRight", rotationMat * ur
+  )
+  
+  frusta_planeCorners = [frusta_planeCorners, planeCorners];
   
 endfor
+
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 #create XML string by loading a Paraview .pvx file, and modify the relevant values:
 
-filename_toMod = strcat( file_path, "/dome_arena.pvx")
+filename_toMod = strcat( file_path, "/frusta_PlaneCorners_CAVE_template.pvx");
 ## These three lines are equivalent to xDoc_in = xmlread(filename_in) in Matlab
 parser_out = javaObject("org.apache.xerces.parsers.DOMParser");
 parser_out.parse(filename_toMod);
 xDoc_out = parser_out.getDocument();
 
 
-numFrusta_out = xDoc_out.getElementsByTagName("Machine").getLength()
+numFrusta_out = xDoc_out.getElementsByTagName("Machine").getLength();
 
 if(numel(frusta_FOV_Euler) != numFrusta_out)
  error("in/out frustum count do not match!")
@@ -117,23 +145,29 @@ for frustumIndex_out = 0 : (numFrusta_out -1 )
   machineElem = xDoc_out.getElementsByTagName("Machine").item(frustumIndex_out);
   
   hostname = machineElem.getAttributes().getNamedItem("Name").getNodeValue()
-  
   # As be cannot rely on the order of the data thx .pvx file,
   # and as machine names are only available as attributes,
   # we have to extract the index: "arenart" are 7 digits.
   hostIndex= str2num( hostname(8))
   
-  LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
-  LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
-  UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
+  planeCorners =  frusta_planeCorners(hostIndex);
+  LowerLeft_string  = num2str( planeCorners.LowerLeft' )
+  LowerRight_string = num2str( planeCorners.LowerRight' )
+  UpperRight_string = num2str( planeCorners.UpperRight' )
   
-  machineElem.getAttributes().getNamedItem("LowerLeft").setNodeValue("1 2 3")
-  machineElem.getAttributes().getNamedItem("LowerRight").setNodeValue("4 5 6")
-  machineElem.getAttributes().getNamedItem("UpperRight").setNodeValue("7 8 9")
+  #TODO
   
-  LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
-  LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
-  UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
+  #LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
+  #LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
+  #UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
+  
+  machineElem.getAttributes().getNamedItem("LowerLeft").setNodeValue(LowerLeft_string);
+  machineElem.getAttributes().getNamedItem("LowerRight").setNodeValue(LowerRight_string);
+  machineElem.getAttributes().getNamedItem("UpperRight").setNodeValue(UpperRight_string);
+  
+  #LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
+  #LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
+  #UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
   #hostname = strcat("arenart", int2str(frustumIndex_in));
     
 endfor 
@@ -147,7 +181,7 @@ xmlString_out = strWriter.toString();
 #-------------------------------------------------------------------------------
 # write the XML string to file:
 
-filename_out = strcat( file_path, "/frusta_PlaneCorners.xml");
+filename_out = strcat( file_path, "/frusta_PlaneCorners_dome_out.pvx");
 
 fid = fopen (filename_out, "w");
 if (fid == -1)
