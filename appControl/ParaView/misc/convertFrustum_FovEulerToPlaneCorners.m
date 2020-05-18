90426c680b4f812fb4a45f53e0310326fb727bf2
Markus Schlueter <mschlueter@geomar.de>
2020-05-18 17:14:54 +0200

    for the otherwise terse language of octave, the java-based XML parsing, reading, modifying, serializing and writing take 160 lines of code. not a bit of math so far..

--- appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
+++ appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
@@ -1,25 +1,37 @@
+# Script to parse view frustum data from an xml file
+# (an SGCT config file used in OpenSpace, to pe precise.),
+# converts them vom FOV+EulerAngles(YawPitchRoll/YXZ) representation
+# to "3 corners of the projection plane" representation.
+# The result is written into another xml file
+# (a ParaView .pvx file).
+#
+# Input is assumed to be in the order: arenamaster, arenart1 .. arenart5.
+# Input Output can have any order, but is assumed to contain 
+# Elements of type <Machine Name="arenart<n>" > Elements, n in 1..5
+
+#-------------------------------------------------------------------------------
 
 javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xercesImpl.jar")
 javaaddpath ("D:/devel/xerces_java/xerces-2_12_1/xml-apis.jar")
 
+# directory of this script
 file_path = fileparts(mfilename('fullpath'))
 
-filename = strcat( file_path, "/openspace_sgct_config.xml")
 
+filename_in = strcat( file_path, "/openspace_sgct_config.xml")
+## These three lines are equivalent to xDoc_in = xmlread(filename_in) in Matlab
+parser_in = javaObject("org.apache.xerces.parsers.DOMParser");
+parser_in.parse(filename_in);
+xDoc_in = parser_in.getDocument();
 
-## These three lines are equivalent to xDoc = xmlread(filename) in Matlab
-parser = javaObject("org.apache.xerces.parsers.DOMParser");
-parser.parse(filename);
-xDoc = parser.getDocument();
+numFrusta_in = xDoc_in.getElementsByTagName("PlanarProjection").getLength();
 
-numFrusta = xDoc.getElementsByTagName("PlanarProjection").getLength();
-
-frusta_FoVEuler = [] ;
+frusta_FOV_Euler = [] ;
 
 # skip index 0, as we are not interested in the master node
-for frustumIndex = 1 : (numFrusta-1)
+for frustumIndex = 1 : (numFrusta_in-1)
   
-  planarProjElem = xDoc.getElementsByTagName("PlanarProjection").item(frustumIndex) ;
+  planarProjElem = xDoc_in.getElementsByTagName("PlanarProjection").item(frustumIndex);
  
   childNodes = planarProjElem.getChildNodes();
 
@@ -58,17 +70,91 @@ for frustumIndex = 1 : (numFrusta-1)
     
   endfor
 
-  frustum_FoVEuler = struct("fov", fov, "eulerAngles", eulerAngles)
+  frustum_FOVEuler = struct("fov", fov, "eulerAngles", eulerAngles)
   
-  frusta_FoVEuler = [frusta_FoVEuler, frustum_FoVEuler]
+  frusta_FOV_Euler = [frusta_FOV_Euler, frustum_FOVEuler]
   
 endfor
 
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+# do the conversions :
 
 
 
+for frustumIndex_in = 0 : numFrusta_out
 
-#theta_degrees = 90 
-#phi_degrees = 0 
 
-#point_1 = sph2cart(theta_degrees/180*pi, phi_degrees/180*pi, 1)
+
+  
+  
+  
+
+  
+endfor
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+#create XML string by loading a Paraview .pvx file, and modify the relevant values:
+
+filename_toMod = strcat( file_path, "/dome_arena.pvx")
+## These three lines are equivalent to xDoc_in = xmlread(filename_in) in Matlab
+parser_out = javaObject("org.apache.xerces.parsers.DOMParser");
+parser_out.parse(filename_toMod);
+xDoc_out = parser_out.getDocument();
+
+
+numFrusta_out = xDoc_out.getElementsByTagName("Machine").getLength()
+
+if(numel(frusta_FOV_Euler) != numFrusta_out)
+ error("in/out frustum count do not match!")
+endif
+
+for frustumIndex_out = 0 : (numFrusta_out -1 )
+  
+  machineElem = xDoc_out.getElementsByTagName("Machine").item(frustumIndex_out);
+  
+  hostname = machineElem.getAttributes().getNamedItem("Name").getNodeValue()
+  
+  # As be cannot rely on the order of the data thx .pvx file,
+  # and as machine names are only available as attributes,
+  # we have to extract the index: "arenart" are 7 digits.
+  hostIndex= str2num( hostname(8))
+  
+  LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
+  LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
+  UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
+  
+  machineElem.getAttributes().getNamedItem("LowerLeft").setNodeValue("1 2 3")
+  machineElem.getAttributes().getNamedItem("LowerRight").setNodeValue("4 5 6")
+  machineElem.getAttributes().getNamedItem("UpperRight").setNodeValue("7 8 9")
+  
+  LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
+  LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
+  UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
+  #hostname = strcat("arenart", int2str(frustumIndex_in));
+    
+endfor 
+
+serializer = javaObject("org.apache.xml.serialize.XMLSerializer");
+strWriter  = javaObject("java.io.StringWriter");
+serializer.setOutputCharStream(strWriter);
+serializer.serialize(xDoc_out);
+xmlString_out = strWriter.toString();
+
+#-------------------------------------------------------------------------------
+# write the XML string to file:
+
+filename_out = strcat( file_path, "/frusta_PlaneCorners.xml");
+
+fid = fopen (filename_out, "w");
+if (fid == -1)
+  error ("Unable to open file ", filename_out,"  Aborting...\n");
+endif
+fprintf(fid, "%s\n", xmlString_out);
+fflush(fid);
+fclose (fid);
+
+#-------------------------------------------------------------------------------
