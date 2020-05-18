89e6e56c1ab5fc305a3f593acff0861ca72d8834
Markus Schlueter <mschlueter@geomar.de>
2020-05-18 17:04:16 +0200

    all five dome nodes are involved in paraview default profile now

--- appControl/ParaView/profiles/default/bin/launchClusterServers.ps1
+++ appControl/ParaView/profiles/default/bin/launchClusterServers.ps1
@@ -7,4 +7,4 @@
 # --mpi 
 # --force-onscreen-rendering 
 
-mpiexec.exe -np 3 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
+mpiexec.exe -np 5 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
