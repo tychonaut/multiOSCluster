010010fc10f982a963776e34fb0854f98409d2f8
Markus Schlueter <mschlueter@geomar.de>
2020-05-19 18:13:48 +0200

    now the stuff works in paraview, for reasons that atm make no sense to me: The order of matrix multiplication in the working octave script must be opposite to that of OpenGL, and is also opposite to the order of quaternion concatenation in OpenSpace. This seems contradictory to me atm; but Paraview works in the dome!

--- appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
+++ appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
@@ -51,11 +51,11 @@ for frustumIndex = 1 : (numFrusta_in-1)
       fovAttribs = childNode.getAttributes();
       
       fov = struct ( 
-        "up",     fovAttribs.getNamedItem("up").getNodeValue(),
-        "down",   fovAttribs.getNamedItem("down").getNodeValue(),
-        "left",   fovAttribs.getNamedItem("left").getNodeValue(),
-        "right",  fovAttribs.getNamedItem("right").getNodeValue()
-      )
+        "up",     str2double(fovAttribs.getNamedItem("up").getNodeValue()),
+        "down",   str2double(fovAttribs.getNamedItem("down").getNodeValue()),
+        "left",   str2double(fovAttribs.getNamedItem("left").getNodeValue()),
+        "right",  str2double(fovAttribs.getNamedItem("right").getNodeValue())
+      );
       
     endif
     
@@ -64,16 +64,16 @@ for frustumIndex = 1 : (numFrusta_in-1)
       dirAttribs = childNode.getAttributes();
       
       eulerAngles = struct (
-        "yaw",    dirAttribs.getNamedItem("heading").getNodeValue(),
-        "pitch",  dirAttribs.getNamedItem("pitch").getNodeValue(),
-        "roll",   dirAttribs.getNamedItem("roll").getNodeValue()
-      )
+        "yaw",    str2double(dirAttribs.getNamedItem("heading").getNodeValue()),
+        "pitch",  str2double(dirAttribs.getNamedItem("pitch").getNodeValue()),
+        "roll",   str2double(dirAttribs.getNamedItem("roll").getNodeValue())
+      );
       
     endif
     
   endfor
 
-  frustum_FOV_Euler = struct("fov", fov, "eulerAngles", eulerAngles);
+  frustum_FOV_Euler = struct("fov", fov, "eulerAngles", eulerAngles)
   
   frusta_FOV_Euler = [frusta_FOV_Euler, frustum_FOV_Euler];
   
@@ -99,10 +99,10 @@ for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
  
   printf("frustum #: %d\n", frustumIndex_in)
  
-  fovU = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.up));
-  fovD = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.down));
-  fovL = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.left));
-  fovR = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).fov.right));
+  fovU = deg2rad(frusta_FOV_Euler(frustumIndex_in).fov.up);
+  fovD = deg2rad(frusta_FOV_Euler(frustumIndex_in).fov.down);
+  fovL = deg2rad(frusta_FOV_Euler(frustumIndex_in).fov.left);
+  fovR = deg2rad(frusta_FOV_Euler(frustumIndex_in).fov.right);
 
   ll = [ -tan(fovL), -tan(fovD), -1.0]' ;
   lr = [  tan(fovR), -tan(fovD), -1.0]' ;
@@ -121,27 +121,27 @@ for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
   mainDir *= 6.0;
   
   #yaw pitch roll -> rotate about y,x,z
-  yaw =   deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.yaw))
-  pitch = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.pitch));
-  roll =  deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.roll));
+  yaw =   deg2rad((frusta_FOV_Euler(frustumIndex_in).eulerAngles.yaw));
+  pitch = deg2rad((frusta_FOV_Euler(frustumIndex_in).eulerAngles.pitch));
+  roll =  deg2rad((frusta_FOV_Euler(frustumIndex_in).eulerAngles.roll));
   
   ## TEST TO MAKE SENSE OF OPENSPACE'S AND OTHER'S HANDEDNESS,
   ## see glm::quat sgct_core::ReadConfig::parseOrientationNode(tinyxml2::XMLElement* element)
   yaw *= -1.0;
-  #pitch *= -1.0; NOT negate
+  #pitch *= -1.0; #NOT negate
   roll *= -1.0;
   
-  #desparation         
-  #domeTiltMat =  createRotationOx( deg2rad(21.0) );
   
   yawMat   = createRotationOy( yaw );
   pitchMat = createRotationOx( pitch );
   rollMat  = createRotationOz( roll );
   
-  ## original try: yxz -> yaw pitch roll
+  ## original try: yxz -> yaw pitch roll; that's how OpenSpace does it
   #rotationMat = rollMat * pitchMat * yawMat;
-  #next try: xyz: pitch yaw roll
-  rotationMat = rollMat * yawMat * pitchMat ;
+  #next try: xyz: pitch yaw roll; looks good in plot, NEARLY correct in Paraview...
+  #rotationMat = rollMat * yawMat * pitchMat ;
+  #desparation: invert order:yxz -> zxy
+  rotationMat = yawMat * pitchMat *  rollMat;
   
   # 4x4 -> 3x3
   rotationMat = rotationMat(1:3, 1:3) 
@@ -152,6 +152,7 @@ for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
   ur = rotationMat * ur;
   
   mainDir = rotationMat * mainDir;
+  
   origin = [0 0 0]';
   
   #DEBUG TEST: compensate for the hypothetical non-[0 0 0 ]'-camaera origin
@@ -240,23 +241,16 @@ for frustumIndex_out = 0 : (numFrusta_out -1 )
   LowerRight_string = num2str( planeCorners.LowerRight' )
   UpperRight_string = num2str( planeCorners.UpperRight' )
   
-  #TODO
-  
-  #LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
-  #LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
-  #UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
   
   machineElem.getAttributes().getNamedItem("LowerLeft").setNodeValue(LowerLeft_string);
   machineElem.getAttributes().getNamedItem("LowerRight").setNodeValue(LowerRight_string);
   machineElem.getAttributes().getNamedItem("UpperRight").setNodeValue(UpperRight_string);
-  
-  
-  #LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
-  #LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
-  #UpperRight_out = machineElem.getAttributes().getNamedItem("UpperRight").getNodeValue()
-  #hostname = strcat("arenart", int2str(frustumIndex_in));
     
 endfor 
+
+
+#-------------------------------------------------------------------------------
+# write the XML string to file:
 
 serializer = javaObject("org.apache.xml.serialize.XMLSerializer");
 strWriter  = javaObject("java.io.StringWriter");
@@ -264,8 +258,7 @@ serializer.setOutputCharStream(strWriter);
 serializer.serialize(xDoc_out);
 xmlString_out = strWriter.toString();
 
-#-------------------------------------------------------------------------------
-# write the XML string to file:
+
 
 filename_out = strcat( file_path, "/frusta_PlaneCorners_dome_out.pvx");
 
