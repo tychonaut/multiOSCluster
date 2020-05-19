a27c517af8eff921c4c6bbb026d6422cc2d761de
Markus Schlueter <mschlueter@geomar.de>
2020-05-19 15:46:22 +0200

    SAVEPOINT

--- appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
+++ appControl/ParaView/misc/convertFrustum_FovEulerToPlaneCorners.m
@@ -84,6 +84,15 @@ endfor
 
 # do the conversions :
 
+#for plotting:
+clf # clear figure
+hold on
+xlabel("x")
+ylabel("y")
+zlabel("z")
+axis("equal")
+
+plotData = [];
 
 frusta_planeCorners = [];
 for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
@@ -97,23 +106,29 @@ for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
 
   ll = [ -tan(fovL), -tan(fovD), -1.0]' ;
   lr = [  tan(fovR), -tan(fovD), -1.0]' ;
+  ul = [ -tan(fovL),  tan(fovU), -1.0]' ;
   ur = [  tan(fovR),  tan(fovU), -1.0]' ;
   
-  #DEBUG TEST
-  ll /= 3.0;
-  lr /= 3.0;
-  ur /= 3.0;
+  
+  mainDir = [0, 0, -1.0]' ;
+  
+  #DEBUG TEST: 3m radius, like in dome
+  ll *= 3.0;
+  lr *= 3.0;
+  ul *= 3.0;
+  ur *= 3.0;
+  
+  mainDir *= 3.0;
   
   #yaw pitch roll -> rotate about y,x,z
   yaw =   deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.yaw))
-  ## TEST TO MAKE SENSE OF OPENSPACE'S AND OTHER'S HANDEDNESS
-  yaw *= -1.0;
-                                                                                 
   pitch = deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.pitch));
-  ## TEST TO MAKE SENSE OF OPENSPACE'S AND OTHER'S HANDEDNESS
-  #pitch *= -1.0;
   roll =  deg2rad(str2double(frusta_FOV_Euler(frustumIndex_in).eulerAngles.roll));
-  ## TEST TO MAKE SENSE OF OPENSPACE'S AND OTHER'S HANDEDNESS
+  
+  ## TEST TO MAKE SENSE OF OPENSPACE'S AND OTHER'S HANDEDNESS,
+  ## see glm::quat sgct_core::ReadConfig::parseOrientationNode(tinyxml2::XMLElement* element)
+  yaw *= -1.0;
+  #pitch *= -1.0;
   roll *= -1.0;
   
   #desparation         
@@ -123,18 +138,67 @@ for frustumIndex_in = 1 : numel(frusta_FOV_Euler)
   pitchMat = createRotationOx( pitch );
   rollMat  = createRotationOz( roll );
   
-  rotationMat = rollMat * pitchMat * yawMat; #* domeTiltMat;
-  rotationMat = rotationMat(1:3,1:3) 
+  rotationMat = rollMat * pitchMat * yawMat;
+  # 4x4 -> 3x3
+  rotationMat = rotationMat(1:3, 1:3) 
+  
+  ll = rotationMat * ll;
+  lr = rotationMat * lr;
+  ul = rotationMat * ul;
+  ur = rotationMat * ur;
+  
+  mainDir = rotationMat * mainDir;
+  origin = [0 0 0]';
+  
+  #DEBUG TEST: compensate for the hypothetical non-[0 0 0 ]'-camaera origin
+  #ll -= [0 0 0.5]';
+  #lr -= [0 0 0.5]';
+  #ur -= [0 0 0.5]';
   
   planeCorners = struct(
-    "LowerLeft",  rotationMat * ll,
-    "LowerRight", rotationMat * lr,
-    "UpperRight", rotationMat * ur
+    "LowerLeft",  ll,
+    "LowerRight", lr,
+    "UpperLeft", ul,
+    "UpperRight", ur,
+    "mainDir", mainDir,
+    "screenFrame_xs", [ ll(1), lr(1), ur(1), ul(1), ll(1) ],
+    "screenFrame_ys", [ ll(2), lr(2), ur(2), ul(2), ll(2) ],
+    "screenFrame_zs", [ ll(3), lr(3), ur(3), ul(3), ll(3) ]
   )
   
   frusta_planeCorners = [frusta_planeCorners, planeCorners];
   
+  #plot stuff
+  i = frustumIndex_in;
+  plot3( 
+     
+    [0, planeCorners.mainDir(1)],    [0,planeCorners.mainDir(2)],    [0,planeCorners.mainDir(3)] , "",
+    [0, planeCorners.LowerLeft(1)],  [0,planeCorners.LowerLeft(2)],  [0,planeCorners.LowerLeft(3)] , "",
+    [0, planeCorners.LowerRight(1)], [0,planeCorners.LowerRight(2)], [0,planeCorners.LowerRight(3)] , "",
+    [0, planeCorners.UpperLeft(1)],  [0,planeCorners.UpperLeft(2)],  [0,planeCorners.UpperLeft(3)] , "",
+    [0, planeCorners.UpperRight(1)], [0,planeCorners.UpperRight(2)], [0,planeCorners.UpperRight(3)] , "",
+    
+    planeCorners.screenFrame_xs, planeCorners.screenFrame_ys, planeCorners.screenFrame_zs, "-"
+  )
+  
+
+  
 endfor
+
+
+
+#  plot3( 
+#  
+#    [0, planeCorners(1).mainDir(1)], [0,planeCorners(1).mainDir(2)], [0,planeCorners(1).mainDir(3)] , "",
+#    [0, planeCorners(1).LowerLeft(1)], [0,planeCorners(1).LowerLeft(2)], [0,planeCorners(1).LowerLeft(3)] , "",
+#    [0, planeCorners(1).LowerRight(1)], [0,planeCorners(1).LowerRight(2)], [0,planeCorners(1).LowerRight(3)] , "",
+#    [0, planeCorners(1).UpperLeft(1)], [0,planeCorners(1).UpperLeft(2)], [0,planeCorners(1).UpperLeft(3)] , "",
+#    [0, planeCorners(1).UpperRight(1)], [0,planeCorners(1).UpperRight(2)], [0,planeCorners(1).UpperRight(3)] , "",
+#    
+#    planeCorners(1).screenFrame_xs, planeCorners(1).screenFrame_ys, planeCorners(1).screenFrame_zs, ""
+#  )
+  
+
 
 
 #-------------------------------------------------------------------------------
@@ -179,6 +243,7 @@ for frustumIndex_out = 0 : (numFrusta_out -1 )
   machineElem.getAttributes().getNamedItem("LowerLeft").setNodeValue(LowerLeft_string);
   machineElem.getAttributes().getNamedItem("LowerRight").setNodeValue(LowerRight_string);
   machineElem.getAttributes().getNamedItem("UpperRight").setNodeValue(UpperRight_string);
+  
   
   #LowerLeft_out = machineElem.getAttributes().getNamedItem("LowerLeft").getNodeValue()
   #LowerRight_out = machineElem.getAttributes().getNamedItem("LowerRight").getNodeValue()
