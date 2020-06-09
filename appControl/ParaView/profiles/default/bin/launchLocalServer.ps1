3ac9452dc2f42532f299ad5f8ccab006b999b2ba
Markus Schlueter <mschlueter@geomar.de>
2020-06-09 07:00:15 +0200

    integrated a 'local server' mode for testing into the ParaView default profile

---
+++ appControl/ParaView/profiles/default/bin/launchLocalServer.ps1
@@ -0,0 +1,10 @@
+
+#mpiexec.exe -np 2 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
+
+
+#--reverse-connection --client-host=arenamaster
+# --log=myPvserverLog.txt,9
+# --mpi 
+# --force-onscreen-rendering 
+
+mpiexec.exe -np 1 D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\local_server.pvx
