48ee1d3afd3dbf821e70920c58efb6f941d928ae
Markus Schlueter <mschlueter@geomar.de>
2020-08-26 11:50:28 +0200

    revert exclusion of RT4

--- appControl/ParaView/profiles/debugging/bin/launchClusterServers.ps1
+++ appControl/ParaView/profiles/debugging/bin/launchClusterServers.ps1
@@ -7,7 +7,11 @@
 # --mpi 
 # --force-onscreen-rendering 
 
+#this is the actual command; uncomment when all is working again
 mpiexec.exe -np 5 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
+
+#mpiexec.exe -np 4 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines_noRt4.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dome_arena.pvx
+
 
 #mpiexec.exe -np 1 -machinefile D:\apps\ParaView\v5.8.0_CAVE\bin\machines_only1.txt  D:\apps\ParaView\v5.8.0_CAVE\bin\pvserver.exe  D:\apps\ParaView\v5.8.0_CAVE\bin\dbgsrv.pvx
 
